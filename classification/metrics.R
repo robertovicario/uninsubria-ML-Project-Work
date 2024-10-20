32e7e57576e618ed5ca8392d96a65c8c551ccdcb
robertovicario <robertovicario.com@gmail.com>
2024-10-21 00:53:32 +0200

    updates

--- classification/metrics.R
+++ classification/metrics.R
@@ -1,33 +1,33 @@
 accuracy <- function(y_pred, y_test) {
-  val <- mean(y_pred == y_test)
-  return(val)
+  mean(y_pred == y_test, na.rm = TRUE)
 }
 
 precision <- function(y_pred, y_test) {
-  tp <- sum(y_pred == 1 & y_test == 1)
-  fp <- sum(y_pred == 1 & y_test == 0)
-  val <- tp / (tp + fp)
-  return(val)
+  tp <- sum(y_pred == 1 & y_test == 1, na.rm = TRUE)
+  fp <- sum(y_pred == 1 & y_test == 0, na.rm = TRUE)
+  
+  if (tp + fp == 0) return(NA)
+
+  tp / (tp + fp)
 }
 
 recall <- function(y_pred, y_test) {
-  tp <- sum(y_pred == 1 & y_test == 1)
-  fn <- sum(y_pred == 0 & y_test == 1)
-  val <- tp / (tp + fn)
-  return(val)
+  tp <- sum(y_pred == 1 & y_test == 1, na.rm = TRUE)
+  fn <- sum(y_pred == 0 & y_test == 1, na.rm = TRUE)
+  
+  if (tp + fn == 0) return(NA)
+  
+  tp / (tp + fn)
 }
 
 f1_score <- function(y_pred, y_test) {
-  precision <- precision(y_pred, y_test)
-  recall <- recall(y_pred, y_test)
-  val <- 2 * (precision * recall) / (precision + recall)
-  return(val)
+  precision_val <- precision(y_pred, y_test)
+  recall_val <- recall(y_pred, y_test)
+  
+  if (is.na(precision_val) || is.na(recall_val)) return(NA)
+  
+  2 * (precision_val * recall_val) / (precision_val + recall_val)
 }
 
-confusion_matrix <- function(y_pred, y_test) {
-  val <- table(y_test, y_pred)
-  return(val)
-}
-
-save(accuracy, confusion_matrix, precision, recall, f1_score, file = "classification/metrics.RData") # nolint
+save(accuracy, precision, recall, f1_score, file = "classification/metrics.RData") # nolint
 print("Saved: 'classification/metrics.RData'")
