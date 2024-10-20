0153b8ee99b5a58bd2ad2c76704d08da62e80bb2
robertovicario <robertovicario.com@gmail.com>
2024-10-20 02:43:38 +0200

    updates

--- classification/svm.R
+++ classification/svm.R
@@ -5,14 +5,6 @@ load("classification/preprocessing.rData")
 x_train <- scale(x_train_lda)
 x_test <- scale(x_test_lda)
 
-svm_model <- svm(x_train, as.factor(y_train), kernel = "linear")
-y_pred <- predict(svm_model, x_test)
-
-print(paste("Accuracy:", accuracy(y_pred, y_test)))
-print(paste("Precision:", precision(y_pred, y_test)))
-print(paste("Recall:", recall(y_pred, y_test)))
-print(paste("F1-Score:", f1_score(y_pred, y_test)))
-
 cost_vals <- 10^seq(-3, 3, by = 1)
 tuned_model <- tune(svm,
                     train.x = x_train,
@@ -22,8 +14,8 @@ tuned_model <- tune(svm,
                     tunecontrol = tune.control(sampling = "cross", cross = 10))
 print(tuned_model)
 
-best_svm_model <- tuned_model$best.model
-y_pred <- predict(best_svm_model, x_test)
+svm_model <- tuned_model$best.model
+y_pred <- predict(svm_model, x_test)
 
 print(paste("Accuracy:", accuracy(y_pred, y_test)))
 print(paste("Precision:", precision(y_pred, y_test)))
